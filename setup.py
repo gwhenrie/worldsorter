752abe6f37e6e47e43288863ee47661f5b58d207
Graigry Henrie <gwhenrie@gmail.com>
2023-04-13 14:27:52 -0600

    Initial pypi structure.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup 
+
+setup(name='worldsorter',
+      version='0.0.1',
+      description='Markdown/config based world-building system.',
+      url='',
+      author='Graigry Henrie',
+      author_email='',
+      license='MIT',
+      packages=['worldsorter'],
+      zip_safe=True)
