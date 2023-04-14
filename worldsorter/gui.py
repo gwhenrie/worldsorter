fb7096db5c777a5c1ef4e33b445b4c3d3c965b6d
Graigry Henrie <gwhenrie@gmail.com>
2023-04-13 21:16:07 -0600

    Initial import of GUI template

--- worldsorter/gui.py
+++ worldsorter/gui.py
@@ -1,1 +1,16 @@
+from PyQt5 import QtWidgets, uic
+import sys, os
 
+class Main(QtWidgets.QMainWindow):
+    
+    def __init__(self, *args, **kwargs):
+        super(Main, self).__init__(*args, **kwargs)
+
+        ui_path = os.path.dirname(os.path.abspath(__file__))
+        uic.loadUi(os.path.join(ui_path, "main.ui"), self)
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    main = Main()
+    main.show()
+    sys.exit(app.exec_())
